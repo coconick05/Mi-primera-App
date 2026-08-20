df9193a404692c062c9d2faba2e0c0b48a9614b0
coconick05 <nicole.mejiap05@gmail.com>
2026-08-20 13:27:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,6 @@ st.header("Gata negra cool")
 st.write("Miawww Miawwwww Miawwww")
 image = Image.open('gatocool.jpg')
 st.image(image, caption= 'Miawww')
+
+texto = st.text_input('Escribe algo', 'Este es mi texto')
+st.write('El texto escrito es', texto)
